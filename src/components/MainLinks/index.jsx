e097464efb8a63342a126226e9326a4133a909b5
Maxim <panda.gtr007@gmail.com>
2023-03-02 22:50:48 +0600

    add left navbar menu

---
+++ src/components/MainLinks/index.jsx
@@ -0,0 +1,38 @@
+import styles from './MainLinks.scss'
+
+function MainLinks(props){
+   const svg = [
+        {1: '../img/menu__icon/1.svg'},
+        {2: '../img/menu__icon/2.svg'},
+        {3: '../img/menu__icon/3.svg'},
+        {4: '../img/menu__icon/4.svg'},
+        {5: '../img/menu__icon/5.svg'},
+        {6: '../img/menu__icon/6.svg'},
+        {7: '../img/menu__icon/7.svg'},
+        {8: '../img/menu__icon/8.svg'},
+        {9: '../img/menu__icon/9.svg'},
+        {10: '../img/menu__icon/10.svg'},
+        {11: '../img/menu__icon/11.svg'},
+        {12: '../img/menu__icon/12.svg'},
+        {13: '../img/menu__icon/13.svg'},
+        {14: '../img/menu__icon/14.svg'},
+    ]
+   console.log(props.item['id'])
+    return(
+        //!НУжно стилизовать каждый LI css модулями
+                <li style={styles}>
+                    <img src={svg[props.item['id']-1][props.item['id']]} alt="" />
+                    <span>{props.item['name']}</span>
+                </li>
+            
+    );
+}
+
+export default MainLinks;
+
+
+
+
+
+
+
