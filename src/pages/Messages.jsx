69d12cdb212a5519d5702a7f7b2342af59dfda58
Maxim <panda.gtr007@gmail.com>
2023-03-04 18:20:51 +0600

    add pages "messages"

--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -1,15 +1,39 @@
+import ourStyle from './twoColumns.module.scss' //общие стили
+import messages from './Messages.module.scss'
+
 function Messages() {
     return (
-        <div>
-            <section>
+        <div className={ourStyle.content}>
+            <section className={ourStyle.section}>
+
+
+                <div className={messages.search}>
+                    <input type="text" placeholder='Поиск'/>
+
+                    {/* //?тут условия if - если есть что то в инпуте то скрывать эти картинки */}
+                    {/*//? и показывать крестик для того что бы стереть данные ввода */}
+                    {/*//? и на них сделать Link что бы подсветка была */}
+                    <img src="../img/pages/messages/1.svg" alt=''/>
+                    <img src="../img/pages/messages/2.svg" alt=''/>
+                    <img src="../img/pages/messages/3.svg" alt=''/>
+                </div>
                 <article>
-                центр
+                    <br/>
+                    отдельные карточки article
+                    <br/><br/>
                 </article>
+
+
+
             </section>
-            <aside>
-                левое меню
+
+
+            
+            <aside className={ourStyle.aside}>
+                блок фильтрации/навигации
             </aside>
         </div>
+    
     );
 }
 
